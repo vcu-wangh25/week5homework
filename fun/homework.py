922e2dc604d7f59dc3cdf52bc50f7acb0c58d1bc
Helen Wang <wangh25@vcu.edu>
2021-03-09 00:37:03 -0500

    error

--- fun/homework.py
+++ fun/homework.py
@@ -1,11 +1,10 @@
-def find_greatest_number(incoming):
+def find_greatest_number(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    incoming = [11,15,17,19]
-    incoming.sort()
-    return_value = incoming[-1]
+    incoming_list= [11,15,17,25]
+    return_value = max(incoming_list)
     return return_value
     
 
@@ -24,7 +23,7 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-    incoming_list = [1,2,3,-4]
+    incoming_list = [1,2,3,4,-10]
     return_value = sum(incoming_list)
     return return_value
     
@@ -34,7 +33,7 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    incoming_dict = {"dog", "cat", "a", "asdfasdfasdfasdfasdf"}
+    incoming_dict = {"dog", "cat", "ate", "rat"}
     return_value = max(incoming_dict, key=len)
     return return_value
     
